b0eadf08b5535e697d6d64f75dffcf4b69357d2f
Gautier Michelin <gm@ideesculture.com>
2018-02-04 18:37:36 +0100

    large edit ; requires now the Archives plugin for Providence

--- views/main_html.php
+++ views/main_html.php
@@ -1,111 +1,64 @@
 <?php
-/* ----------------------------------------------------------------------
- * app/widgets/links/views/main_html.php : 
+/*
  * ----------------------------------------------------------------------
- * CollectiveAccess
- * Open-source collections management software
+ * CollectiveAccess Archives widget & plugin
+ * Created by idéesculture 2018 (www.ideesculture.com)
  * ----------------------------------------------------------------------
+ * CollectiveAccess is a TM by Whirl-i-Gig
+ * CollectiveAccess and this Archives widget & plugin are under GNU GPL v2 license
  *
- * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
- * Copyright 2010-2016 Whirl-i-Gig
- *
- * For more information visit http://www.CollectiveAccess.org
- *
- * This program is free software; you may redistribute it and/or modify it under
- * the terms of the provided license as published by Whirl-i-Gig
- *
- * CollectiveAccess is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTIES whatsoever, including any implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
- *
- * This source code is free and modifiable under the terms of 
- * GNU General Public License. (http://www.gnu.org/copyleft/gpl.html). See
- * the "license.txt" file for details, or visit the CollectiveAccess web site at
- * http://www.CollectiveAccess.org
- *
- * ----------------------------------------------------------------------
  */
+$root_type_id = $this->getVar("root_type");
  ?>
  <style>
 	.slideDownContent {
-		display:none;	 
+		display:none;
 	}
 	.slideDownContent TR {
 		width:100%;
 	}
 </style>
- <div class="dashboardWidgetContentContainer">
-<?php
- 	$po_request			= $this->getVar('request');
-	 	require_once(__CA_LIB_DIR__."/ca/Search/ObjectSearch.php");
-	 	$o_search = new ObjectSearch(); //instantiate a new search object
-	 	$qr_hits = $o_search->search("ca_objects.type_id:43");
-	 	$count=0;
-		while($qr_hits->nextHit()) {
-			print "<h3><button onclick='$(\"#content".$count."\").slideToggle();$(this).toggleClass(\"opened\");'>+</button><a href=/gestion/index.php/editor/objects/ObjectEditor/Edit/object_id/".$qr_hits->get('object_id').">".$qr_hits->get('ca_objects.preferred_labels.name')."</a> (".$qr_hits->get('ca_objects.idno').")</h3>";  //confirm the id number
-			print "<div id=\"content".$count."\" class=\"slideDownContent\">";
-			$object_id_fabrique =  $qr_hits->get('ca_objects.object_id');
-			$o_data = new Db();
-			 $qr_result = $o_data->query("SELECT ca_object_labels.object_id, idno, name FROM ca_objects left join ca_object_labels ON ca_objects.object_id = ca_object_labels.object_id AND is_preferred=1 WHERE parent_id =".$object_id_fabrique." and deleted=0");
-			 $i=0;
-			 print "<table border=0>";
-			 while($qr_result->nextRow()) {
-			       ?>
-			       <tr>
-				<?php print "<td><button onclick='$(\"#content".$count."-".$i."\").slideToggle();$(this).toggleClass(\"opened\");'>+</button><a href=/gestion/index.php/editor/objects/ObjectEditor/Edit/object_id/".$qr_result->get('object_id').">".$qr_result->get('name')."</a></td><td class='td-right'>".$qr_result->get('idno')."</td>"; ?>
-				<?php
-				$o_data2 = new Db();
-				 $qr_result2 = $o_data2->query("SELECT ca_object_labels.object_id, idno, name FROM ca_objects left join ca_object_labels ON ca_objects.object_id = ca_object_labels.object_id AND is_preferred=1 WHERE parent_id =".$qr_result->get('object_id')." and deleted=0");
-				 $j=0;
-				 print "</tr><tr><td colspan=2><table id=\"content".$count."-".$i."\" style=\"padding-left:30px;\" class=\"slideDownContent\" border=0>";
-				 while($qr_result2->nextRow()) {
-				       ?>
-					<?php print "<tr><td><button onclick='$(\"#content".$count."-".$i."-".$j."\").slideToggle();$(this).toggleClass(\"opened\");'>+</button><a href=/gestion/index.php/editor/objects/ObjectEditor/Edit/object_id/".$qr_result2->get('object_id').">".$qr_result2->get('name')."</a></td><td class='td-right'>".$qr_result2->get('idno');
-						$o_data3 = new Db();
-						$qr_result3 = $o_data3->query("SELECT ca_object_labels.object_id, idno, name FROM ca_objects left join ca_object_labels ON ca_objects.object_id = ca_object_labels.object_id AND is_preferred=1 WHERE parent_id =".$qr_result2->get('object_id')." and deleted=0");
-						$k=0;
-						print "</td></tr>"; 
-						print "<tr><td colspan=2>"; 
-						print "<table id=\"content".$count."-".$i."-".$j."\" style=\"padding-left:30px;width:100%;\" class=\"slideDownContent\" border=0>";
-						$k=0;
-						while($qr_result3->nextRow()) {
-							$k++;
-							print "<tr><td colspan=2><button onclick='$(\"#content".$count."-".$i."-".$j."-".$k."\").slideToggle();$(this).toggleClass(\"opened\");'>+</button> <a href=/gestion/index.php/editor/objects/ObjectEditor/Edit/object_id/".$qr_result3->get('object_id').">".$qr_result3->get('name')."</a></td><td class='td-right'>".$qr_result3->get('idno')."</td></tr>";
-							$o_data4 = new Db();
-							$qr_result4 = $o_data4->query("SELECT ca_object_labels.object_id, idno, name FROM ca_objects left join ca_object_labels ON ca_objects.object_id = ca_object_labels.object_id AND is_preferred=1 WHERE parent_id =".$qr_result3->get('object_id')." and deleted=0");
-							print "<tr><td colspan=3>";
-							print "<div id=\"content".$count."-".$i."-".$j."-".$k."\" style='display:none;'>";
-							while($qr_result4->nextRow()) {
-								print "<a href='/gestion/index.php/editor/objects/ObjectEditor/Edit/object_id/". $qr_result4->get('object_id')."'> ".$qr_result4->get('name')."</a> <span class='pull-right'>".$qr_result4->get('idno')."</span><br/>";
-							}
-							print "</div>";
-							print "</td></tr>";
-						}
-						print "</table>";
-						print "</td></tr>"; 
-						?>
-					<?php
-				   $j++;   
-				 }
-				print"</table></td></tr>";					
-			   $i++;   
-			 }
-			print "</table></div>";
-			$count++;
-		}
- 	
-?>
+<div class="dashboardWidgetContentContainer">
+	<div style="float: right;
+    margin-top: -10px;
+    margin-right: 8px;">
+		<a class="btn" href="/gestion/index.php/archives/archives/Index"><i class="fa fa-expand pullright" aria-hidden="true"></i> Agrandir</a></div>
+
+	<div id="injectArchiveTree">
+		...
+	</div>
 </div>
 <style>
-	button.opened {
-		/*background: gray;*/
-	    text-indent: 1000px;
-	    overflow: hidden;
-	    display: inline-block;
-	    width: 22px;
+	.archiveHierarchyOffset0 {width:75%;padding-left:0;}
+	.archiveHierarchyOffset1 {width:72%;padding-left:3%;}
+	.archiveHierarchyOffset2 {width:69%;padding-left:6%;}
+	.archiveHierarchyOffset3 {width:66%;padding-left:9%;}
+	.archiveHierarchyOffset4 {width:63%;padding-left:12%;}
+	a[id^="expandButton"], a[id^="shrinkButton"] {
+		color:black;
+		cursor:pointer;
 	}
-
-	.td-right {
-		text-align: right;
-	}	
 </style>
+<script>
+	jQuery("document").ready(function() {
+		jQuery.get("/gestion/index.php/archives/archives/Fetch", function(data){
+			jQuery('#injectArchiveTree').html(data);
+		}, "html");
+
+	});
+	var expandHierarchy = function(object_id,level) {
+		jQuery("#expandButton"+object_id).parent().prepend("<a onClick='shrinkHierarchy("+object_id+","+level+");' id='shrinkButton"+object_id+"'><i class='fa fa-minus-square-o'></i></a>");
+		jQuery("#expandButton"+object_id).remove();
+		//jQuery("#expandButton"+object_id+" i.fa").addClass("fa-square-o");
+		//jQuery("#expandButton"+object_id+" i.fa").removeClass("fa-plus-square-o");
+		jQuery.get("<?php print __CA_URL_ROOT__; ?>/index.php/archives/archives/Fetch/id/"+object_id+"/level/"+level, function(data){
+			jQuery("#hierarchyFor"+object_id).html(data);
+		}, "html");
+
+	}
+	var shrinkHierarchy = function(object_id,level) {
+		jQuery("#shrinkButton"+object_id).parent().prepend("<a onClick='expandHierarchy("+object_id+","+level+");' id='expandButton"+object_id+"'><i class='fa fa-plus-square-o'></i></a>");
+		jQuery("#shrinkButton"+object_id).remove();
+		jQuery("#hierarchyFor"+object_id).html("");
+	}
+</script>
